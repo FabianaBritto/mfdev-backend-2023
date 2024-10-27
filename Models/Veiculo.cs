aaf1a85a0fa7a31f0f35d5038381bd0e6b2f8aba
Fabiana <fabianappbritto@gmail.com>
2024-10-27 15:22:37 -0300

    Config Banco de dados e tabela veiculos

---
+++ Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mfdev_backend_2023.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o nome!")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatório informar a Placa!")]
+        public string Placa { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatório informar o ano de fabricação!")]
+        public string AnoFabricacao { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatório informar o ano do modelo!")]
+        public string AnoModelo { get; set; }
+    }
+}
+
